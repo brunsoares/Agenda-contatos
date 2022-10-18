7aaeb41250d9a273ec140d23a84a8e9ff5369a0c
brunsoares <brunohenrique.soares@outlook.com>
2022-10-17 21:58:34 -0300

    APP-01 #Comment
    Alterações Efetuadas:
    - Criação do projeto;
    - Ajuste nas dependencias;
    - Criação do componente contato;
    - Criação do serviço contato;

---
+++ agenda-app/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ContatoComponent } from './contato/contato.component';
+import { ContatoService } from './contato.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContatoComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+  ],
+  providers: [ ContatoService ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
